2c775f9fb16b9dfbd74e6cb1ed4d194c033b96dc
Greg Wong <Gregory.LT.Wong@gmail.com>
2017-08-26 16:37:40 +0800

    components, props, and proptypes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 
+const Headline = () => {
+    return <h1 className="title">Welcome to the React world!</h1>
+}
+
+const Greeting = (props) => {
+    return <p>You will love it {props.name} ({props.age})!</p>
+}
+
+const Message = (props) => {
+    const {name, age} = props;
+    return <p>My name is {name} ({age})!</p>
+}
+
 const App = () => {
     return (
         <div>
-            <h1 className="title">Hello world!</h1>
-            <p>Testing rendering with JSX. It works!</p>
+            <Headline />
+            <Greeting name="John" age="25"/>
+            <Message name="Sam" age={25}/>
         </div>
     )
+}
+
+Message.propTypes = {
+    name: React.PropTypes.string,
+    age: React.PropTypes.number.isRequired
 }
 
 export default App;
