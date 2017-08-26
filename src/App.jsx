5533a79f985f086f1f1f42a907e8664de6250697
Greg Wong <Gregory.LT.Wong@gmail.com>
2017-08-26 16:17:05 +0800

    hack to fix filename case change

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1 className="title">Hello world!</h1>
+            <p>Testing rendering with JSX. It works!</p>
+        </div>
+    )
+}
+
+export default App;
